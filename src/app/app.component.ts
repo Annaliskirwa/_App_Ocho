3a856997d3bca25486be82d51acc8a96db791b25
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-21 15:30:54 +0300

    It will add ghtml template with bootstrap classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Annalis</h1>
-  <p>I dont know es6 </p>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
