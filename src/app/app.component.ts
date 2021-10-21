de0dc8a929429a88764293ae6ba2c8ba57780115
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-21 21:00:29 +0300

    It will initialize an expression value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Artist-Angular';
+  query!: string;
+
+  constructor(){
+    this.query = "Barot"
+  }
 }
