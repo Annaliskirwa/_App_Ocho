065414bef6fe600c9ab278d5a73dbd8ea1471800
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-02 11:36:12 +0300

    It will add the search pipe to the html template

--- src/app/search-artist.pipe.ts
+++ src/app/search-artist.pipe.ts
@@ -8,7 +8,7 @@ export class SearchArtistPipe implements PipeTransform {
   transform(pipeData: pipeModifier): any {
     return pipeData.filter(eachItem => { return (
       eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()) ||
-      eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()) || 
+      eachItem['reknown'].toLowerCase().includes(pipeModifier.toLowerCase())
     )
    });
   }
