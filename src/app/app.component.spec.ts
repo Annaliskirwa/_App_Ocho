108a6521e5e556533dbe5f17a82d27611b6918b3
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-02 18:39:43 +0300

    It will display the artists from data.json

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ArtistAngular'`, () => {
+  it(`should have as title 'Artist-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ArtistAngular');
+    expect(app.title).toEqual('Artist-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ArtistAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Artist-Angular app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ArtistAngular';
+export class AppComponent implements OnInit {
+  query!: string
+  artist: any
+
+  showArtist(item:any){
+    
+    this.query = item.name;
+    item.highlight = !item.highlight
+  }
+
+  constructor(private http: HttpClient){
+    this.query = ""
+    
+  }
+  ngOnInit(): void{
+    this.http.get<Object>('../assets/data.json').subscribe(data =>{
+      this.artist = data
+    })
+  }
 }
